e1233a5ecaad561b7cda3f93ac2b2b9b767f6828
Gary Hull <gary.hull@digital.hmrc.gov.uk>
2017-06-27 15:36:50 +0100

    implemented gross assets inlcuding error page and api call

---
+++ app/controllers/seis/GrossAssetsController.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.seis
+
+import auth.{AuthorisedAndEnrolledForTAVC, SEIS}
+import common.KeystoreKeys
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import connectors.{EnrolmentConnector, S4LConnector, SubmissionConnector}
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+import play.api.mvc._
+import controllers.predicates.FeatureSwitch
+import forms.GrossAssetsForm._
+import models.GrossAssetsModel
+import play.api.Logger
+import play.api.i18n.Messages.Implicits._
+import play.api.Play.current
+import views.html.seis.companyDetails.GrossAssets
+import config.FrontendGlobal._
+
+import scala.concurrent.Future
+
+object GrossAssetsController extends GrossAssetsController {
+  override lazy val s4lConnector = S4LConnector
+  val submissionConnector: SubmissionConnector = SubmissionConnector
+  override lazy val applicationConfig = FrontendAppConfig
+  override lazy val authConnector = FrontendAuthConnector
+  override lazy val enrolmentConnector = EnrolmentConnector
+}
+
+trait GrossAssetsController extends FrontendController with AuthorisedAndEnrolledForTAVC with FeatureSwitch {
+
+  override val acceptedFlows = Seq(Seq(SEIS))
+
+  val submissionConnector: SubmissionConnector
+
+  val show: Action[AnyContent] = featureSwitch(applicationConfig.seisFlowEnabled) {
+    AuthorisedAndEnrolled.async { implicit user => implicit request =>
+
+      s4lConnector.fetchAndGetFormData[GrossAssetsModel](KeystoreKeys.grossAssets).map {
+        case Some(data) => Ok(GrossAssets(grossAssetsForm.fill(data)))
+        case None => Ok(GrossAssets(grossAssetsForm))
+      }
+    }
+  }
+
+  def submit: Action[AnyContent] = featureSwitch(applicationConfig.seisFlowEnabled) {
+    AuthorisedAndEnrolled.async { implicit user => implicit request =>
+
+      def routeRequest(grossAssetsExceeded: Option[Boolean]): Future[Result] = {
+
+        if (grossAssetsExceeded.nonEmpty) {
+          grossAssetsExceeded match {
+            //TODO navigate to FTE page
+            case Some(false) => Future.successful(Redirect(routes.GrossAssetsController.show()))
+            case _ => Future.successful(Redirect(routes.GrossAssetsErrorController.show()))
+          }
+        }
+        else {
+          // no expected true/false from service returned
+          Future.successful(InternalServerError(internalServerErrorTemplate))
+        }
+      }
+
+      grossAssetsForm.bindFromRequest().fold(
+        formWithErrors => {
+          Future.successful(BadRequest(GrossAssets(formWithErrors)))
+        },
+        validFormData => {
+          s4lConnector.saveFormData(KeystoreKeys.grossAssets, validFormData)
+          (for {
+            grossAssetsExceeded <- submissionConnector.checkGrossAssetsAmountExceeded(validFormData)
+            route <- routeRequest(grossAssetsExceeded)
+          } yield route) recover {
+            case e: Exception => {
+              Logger.warn(s"[GrossAssetsController][submit] - submit Exception: ${e.getMessage}")
+              InternalServerError(internalServerErrorTemplate)
+            }
+          }
+        }
+      )
+    }
+  }
+}
---
+++ test/controllers/seis/GrossAssetsControllerSpec.scala
@@ -0,0 +1,127 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.seis
+
+import auth.{MockAuthConnector, MockConfig}
+import common.KeystoreKeys
+import config.{FrontendAppConfig, FrontendAuthConnector}
+import connectors.{EnrolmentConnector, S4LConnector, SubmissionConnector}
+import controllers.helpers.BaseSpec
+import models._
+import org.mockito.Matchers
+import org.mockito.Mockito._
+import play.api.test.Helpers._
+
+import scala.concurrent.Future
+
+class GrossAssetsControllerSpec extends BaseSpec {
+
+  object TestController extends GrossAssetsController {
+    override lazy val applicationConfig = MockConfig
+    override lazy val authConnector = MockAuthConnector
+    override lazy val s4lConnector = mockS4lConnector
+    override lazy val submissionConnector = mockSubmissionConnector
+    override lazy val enrolmentConnector = mockEnrolmentConnector
+  }
+
+  val grossAssets = GrossAssetsModel(12345)
+
+  "GrossAssetsController" should {
+    "use the correct keystore connector" in {
+      GrossAssetsController.s4lConnector shouldBe S4LConnector
+    }
+    "use the correct auth connector" in {
+      GrossAssetsController.authConnector shouldBe FrontendAuthConnector
+    }
+    "use the correct enrolment connector" in {
+      GrossAssetsController.enrolmentConnector shouldBe EnrolmentConnector
+    }
+    "use the correct submission connector" in {
+      GrossAssetsController.submissionConnector shouldBe SubmissionConnector
+    }
+    "use the correct application config" in {
+      GrossAssetsController.applicationConfig shouldBe FrontendAppConfig
+    }
+  }
+
+  "Sending a GET request to GrossAssetsController when authenticated and enrolled" should {
+
+    "return a 200 when something is fetched from keystore" in {
+      mockEnrolledRequest(seisSchemeTypesModel)
+      when(mockS4lConnector.fetchAndGetFormData[GrossAssetsModel](Matchers.eq(KeystoreKeys.grossAssets))
+        (Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Future.successful(Option(grossAssets)))
+      showWithSessionAndAuth(TestController.show)(
+        result => status(result) shouldBe OK
+      )
+    }
+
+    "provide an empty model and return a 200 when nothing is fetched using keystore" in {
+      when(mockS4lConnector.fetchAndGetFormData[GrossAssetsModel](Matchers.eq(KeystoreKeys.grossAssets))
+        (Matchers.any(), Matchers.any(), Matchers.any())).thenReturn(Future.successful(None))
+      mockEnrolledRequest(seisSchemeTypesModel)
+      showWithSessionAndAuth(TestController.show)(
+        result => status(result) shouldBe OK
+      )
+    }
+
+  }
+
+  "Sending a valid form submit to the GrossAssetsController" should {
+    "redirect to the correct next page if the allowed amount is not exceeded from API" in {
+      when(mockSubmissionConnector.checkGrossAssetsAmountExceeded(Matchers.any())
+      (Matchers.any())).thenReturn(Future.successful(Option(false)))
+      mockEnrolledRequest(seisSchemeTypesModel)
+      submitWithSessionAndAuth(TestController.submit,
+        "grossAmount" -> "200000")(
+        result => {
+          status(result) shouldBe SEE_OTHER
+          redirectLocation(result) shouldBe Some(routes.GrossAssetsController.show().url) //TODO: go to next page
+        }
+      )
+    }
+  }
+
+
+  "Sending a valid form submit to the GrossAssetsController" should {
+    "redirect to the correct next page if the allowed amount is exceeded from API" in {
+      when(mockSubmissionConnector.checkGrossAssetsAmountExceeded(Matchers.any())
+      (Matchers.any())).thenReturn(Future.successful(Option(true)))
+      mockEnrolledRequest(seisSchemeTypesModel)
+      submitWithSessionAndAuth(TestController.submit,
+        "grossAmount" -> "2000001")(
+        result => {
+          status(result) shouldBe SEE_OTHER
+          redirectLocation(result) shouldBe Some(routes.GrossAssetsErrorController.show().url)
+        }
+      )
+    }
+  }
+
+  "Sending an invalid form submission with validation errors to the GrossAssetsController" should {
+    "redirect to itself" in {
+      mockEnrolledRequest(seisSchemeTypesModel)
+      submitWithSessionAndAuth(TestController.submit,
+        "grossAmount" -> "")(
+        result => {
+          status(result) shouldBe BAD_REQUEST
+        }
+      )
+    }
+  }
+
+}
+
