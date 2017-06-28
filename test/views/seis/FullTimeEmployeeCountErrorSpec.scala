ba6da52bca74b71b9b8663b4133b4131643c2a03
James Forster <james.forster@digital.hmrc.gov.uk>
2017-06-28 10:17:20 +0100

    Added FullTimeEmployeeCountError view.

---
+++ test/views/seis/FullTimeEmployeeCountErrorSpec.scala
@@ -0,0 +1,97 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package views.seis
+
+import org.jsoup.Jsoup
+import play.api.i18n.Messages
+import play.api.i18n.Messages.Implicits.applicationMessages
+import views.helpers.ViewSpec
+import views.html.seis.companyDetails.FullTimeEmployeeCountError
+
+class FullTimeEmployeeCountErrorSpec extends ViewSpec {
+
+  "The FullTimeEmployeeCountError view" should {
+    lazy val doc = Jsoup.parse(FullTimeEmployeeCountError()(fakeRequest, applicationMessages).body)
+
+    "contain a title with the correct text" in {
+      doc.title() shouldBe Messages("page.seis.companyDetails.fullTimeEmployeeCountError.title")
+    }
+
+    "contain a back link" which {
+      lazy val backLink = doc.select("article a").first()
+
+      "which has the back text" in {
+        backLink.text() shouldBe Messages("common.button.back")
+      }
+
+      //TODO update test name
+      "has a link to TODO" in {
+        backLink.attr("href") shouldBe "TODO"
+      }
+    }
+
+    "contain a heading with the correct text" in {
+      doc.select("h1").text() shouldBe Messages("page.seis.companyDetails.fullTimeEmployeeCountError.title")
+    }
+
+    "contain a paragraph explaining the error" in {
+      doc.select("article p").first().text() shouldBe Messages("page.seis.companyDetails.fullTimeEmployeeCountError.error")
+    }
+
+    "contain a sub-heading for what to do next" in {
+      doc.select("h2").text() should include(Messages("page.seis.companyDetails.fullTimeEmployeeCountError.whatNext"))
+    }
+
+    "contain a paragraph explaining what actions should be taken" in {
+      doc.select("article p").get(1).text() shouldBe Messages("page.seis.companyDetails.fullTimeEmployeeCountError.guidance")
+    }
+
+    "contain a continue link" which {
+      lazy val link = doc.select("article div > a")
+
+      "appears as a button" in {
+        link.attr("class") shouldBe "button"
+      }
+
+      //TODO update test name
+      "has a link to TODO" in {
+        link.attr("href") shouldBe "TODO"
+      }
+
+      "has the text continue" in {
+        link.text() shouldBe Messages("common.button.continue")
+      }
+    }
+
+    "contain a change link" which {
+      lazy val change = doc.select("article div span")
+
+      "has the correct guidance text" in {
+        change.text() shouldBe s"${Messages("page.seis.companyDetails.fullTimeEmployeeCountError.incorrect")} ${Messages("page.seis.companyDetails.fullTimeEmployeeCountError.incorrect.link")}."
+      }
+
+      //TODO update test name
+      "has a link with an href to TODO" in {
+        change.select("a").attr("href") shouldBe "TODO"
+      }
+
+      "has the correct link component text" in {
+        change.select("a").text() shouldBe Messages("page.seis.companyDetails.fullTimeEmployeeCountError.incorrect.link")
+      }
+    }
+  }
+}
