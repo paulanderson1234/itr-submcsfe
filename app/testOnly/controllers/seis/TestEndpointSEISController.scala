6077a8c9c0067163181395e82bc0a0189f69fe12
david.gordon <david.gordon@digital.hmrc.gov.uk>
2017-07-10 17:05:44 +0100

    TAVC-3170 Added test endpoint

--- app/testOnly/controllers/seis/TestEndpointSEISController.scala
+++ app/testOnly/controllers/seis/TestEndpointSEISController.scala
@@ -147,10 +147,12 @@ trait TestEndpointSEISController extends FrontendController with AuthorisedAndEn
       for {
         numberOfSharesForm <- fillForm[NumberOfSharesModel](KeystoreKeys.numberOfShares, NumberOfSharesForm.numberOfSharesForm)
         nominalValueOfSharesForm <- fillForm[NominalValueOfSharesModel](KeystoreKeys.nominalValueOfShares, NominalValueOfSharesForm.nominalValueOfSharesForm)
+        shareDescription <- fillForm[ShareDescriptionModel](KeystoreKeys.shareDescription, ShareDescriptionForm.shareDescriptionForm)
       } yield Ok(
         testOnly.views.html.seis.testEndpointSEISPageTwo(
           numberOfSharesForm,
-          nominalValueOfSharesForm
+          nominalValueOfSharesForm,
+          shareDescription
         )
       )
   }
@@ -158,12 +160,14 @@ trait TestEndpointSEISController extends FrontendController with AuthorisedAndEn
   def submitPageTwo: Action[AnyContent] = AuthorisedAndEnrolled.async { implicit user => implicit request =>
     val numberOfShares = bindForm[NumberOfSharesModel](KeystoreKeys.numberOfShares, NumberOfSharesForm.numberOfSharesForm)
     val nominalValueOfShares = bindForm[NominalValueOfSharesModel](KeystoreKeys.nominalValueOfShares, NominalValueOfSharesForm.nominalValueOfSharesForm)
+    val shareDescription = bindForm[ShareDescriptionModel](KeystoreKeys.shareDescription, ShareDescriptionForm.shareDescriptionForm)
     saveBackLinks()
     saveSchemeType()
     Future.successful(Ok(
       testOnly.views.html.seis.testEndpointSEISPageTwo(
         numberOfShares,
-        nominalValueOfShares
+        nominalValueOfShares,
+        shareDescription
       )
     ))
   }
