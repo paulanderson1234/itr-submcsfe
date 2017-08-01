f31a46e3d3cd408a85da497792dee019c3ddd403
James Forster <james.forster@digital.hmrc.gov.uk>
2017-08-01 10:04:41 +0100

    Refactor controller logic to increase reusability and simplify nesting.

--- app/controllers/Helpers/ControllerHelpers.scala
+++ app/controllers/Helpers/ControllerHelpers.scala
@@ -18,23 +18,20 @@ package controllers.Helpers
 
 import auth.TAVCUser
 import common.{Constants, KeystoreKeys}
-import forms.CompanyOrIndividualForm._
+import controllers.seis.routes
 import models._
-import models.investorDetails.InvestorDetailsModel
+import models.investorDetails.{InvestorDetailsModel, PreviousShareHoldingModel}
 import models.submission.SchemeTypesModel
 import play.api.data.Form
-import uk.gov.hmrc.play.http.HeaderCarrier
 import play.api.i18n.Messages
-import play.api.mvc.{AnyContent, Request, Result}
-import views.html.seis.companyDetails.QualifyBusinessActivity_Scope0.QualifyBusinessActivity_Scope1.QualifyBusinessActivity
-import common.Constants._
-import controllers.seis.routes
 import play.api.mvc.Results._
+import play.api.mvc.{AnyContent, Request, Result}
+import uk.gov.hmrc.play.http.HeaderCarrier
 
 import scala.concurrent.ExecutionContext.Implicits.global
 import scala.concurrent.Future
 
-object  ControllerHelpers extends ControllerHelpers {
+object ControllerHelpers extends ControllerHelpers {
 
 }
 
@@ -49,15 +46,15 @@ trait ControllerHelpers {
   }
 
   /** Helper method to return the correct trade start date depending on whether the business activity is tarde or R&D.
-    *  A single TradeStartDateModel is returned form checking S4L.
-    *  If expected values are not found None is returned
+    * A single TradeStartDateModel is returned form checking S4L.
+    * If expected values are not found None is returned
     *
     * @param s4lConnector An instance of the Save4Later Connector.
     */
   def getTradeStartDateForBusinessActivity(s4lConnector: connectors.S4LConnector)
                                           (implicit hc: HeaderCarrier, user: TAVCUser): Future[Option[HasInvestmentTradeStartedModel]] = {
 
-    def getDate(businessActivity:Option[QualifyBusinessActivityModel],
+    def getDate(businessActivity: Option[QualifyBusinessActivityModel],
                 researchDateStarted: Option[ResearchStartDateModel],
                 tradeStartDateStarted: Option[HasInvestmentTradeStartedModel]) = {
 
@@ -88,19 +85,19 @@ trait ControllerHelpers {
   def routeToScheme(schemeTypesModel: SchemeTypesModel)(implicit request: Request[AnyContent]): String = {
     schemeTypesModel match {
       //EIS Flow
-      case SchemeTypesModel(true,false,false,false) => controllers.eis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(true, false, false, false) => controllers.eis.routes.NatureOfBusinessController.show().url
       //SEIS Flow
-      case SchemeTypesModel(false,true,false,false) => controllers.seis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(false, true, false, false) => controllers.seis.routes.NatureOfBusinessController.show().url
       //VCT Flow
-      case SchemeTypesModel(false,false,false,true) => controllers.eis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(false, false, false, true) => controllers.eis.routes.NatureOfBusinessController.show().url
       //EIS SEIS Flow
-      case SchemeTypesModel(true,true,false,false) => controllers.eisseis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(true, true, false, false) => controllers.eisseis.routes.NatureOfBusinessController.show().url
       //EIS VCT Flow
-      case SchemeTypesModel(true,false,false,true) => controllers.eis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(true, false, false, true) => controllers.eis.routes.NatureOfBusinessController.show().url
       //SEIS VCT Flow
-      case SchemeTypesModel(false,true,false,true) => controllers.eisseis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(false, true, false, true) => controllers.eisseis.routes.NatureOfBusinessController.show().url
       //EIS SEIS VCT Flow
-      case SchemeTypesModel(true,true,false,true) => controllers.eisseis.routes.NatureOfBusinessController.show().url
+      case SchemeTypesModel(true, true, false, true) => controllers.eisseis.routes.NatureOfBusinessController.show().url
       //Assume EIS
       case _ => controllers.eis.routes.NatureOfBusinessController.show().url
     }
@@ -109,27 +106,27 @@ trait ControllerHelpers {
   def schemeDescriptionFromTypes(schemeTypesModel: Option[SchemeTypesModel])(implicit request: Request[AnyContent], messages: Messages): String = {
     schemeTypesModel match {
       //EIS Flow
-      case Some(SchemeTypesModel(true,false,false,false)) => Messages("page.introduction.hub.existing.advanced.assurance.type")
+      case Some(SchemeTypesModel(true, false, false, false)) => Messages("page.introduction.hub.existing.advanced.assurance.type")
       //SEIS Flow
-      case Some(SchemeTypesModel(false,true,false,false)) => Messages("page.introduction.hub.existing.seis.type")
+      case Some(SchemeTypesModel(false, true, false, false)) => Messages("page.introduction.hub.existing.seis.type")
       //VCT Flow
-      case Some(SchemeTypesModel(false,false,false,true)) => Messages("page.introduction.hub.existing.vct.type")
+      case Some(SchemeTypesModel(false, false, false, true)) => Messages("page.introduction.hub.existing.vct.type")
       //EIS SEIS Flow
-      case Some(SchemeTypesModel(true,true,false,false)) => Messages("page.introduction.hub.existing.eis-seis.type")
+      case Some(SchemeTypesModel(true, true, false, false)) => Messages("page.introduction.hub.existing.eis-seis.type")
       //EIS VCT Flow
-      case Some(SchemeTypesModel(true,false,false,true)) => Messages("page.introduction.hub.existing.eis-vct.type")
+      case Some(SchemeTypesModel(true, false, false, true)) => Messages("page.introduction.hub.existing.eis-vct.type")
       //SEIS VCT Flow
-      case Some(SchemeTypesModel(false,true,false,true)) => Messages("page.introduction.hub.existing.seis-vct.type")
+      case Some(SchemeTypesModel(false, true, false, true)) => Messages("page.introduction.hub.existing.seis-vct.type")
       //EIS SEIS VCT Flow
-      case Some(SchemeTypesModel(true,true,false,true)) => Messages("page.introduction.hub.existing.eis-seis-vct.type")
+      case Some(SchemeTypesModel(true, true, false, true)) => Messages("page.introduction.hub.existing.eis-seis-vct.type")
       //Assume EIS
       case Some(_) => Messages("page.introduction.hub.existing.advanced.assurance.type")
       //Assume EIS
-      case None =>  Messages("page.introduction.hub.existing.advanced.assurance.type")
+      case None => Messages("page.introduction.hub.existing.advanced.assurance.type")
     }
   }
 
-  def useInvestorOrNomineeValueAsHeadingText(investorOrNominee: AddInvestorOrNomineeModel): String =  investorOrNominee.addInvestorOrNominee.toLowerCase
+  def useInvestorOrNomineeValueAsHeadingText(investorOrNominee: AddInvestorOrNomineeModel): String = investorOrNominee.addInvestorOrNominee.toLowerCase
 
   def redirectNoInvestors(vector: Option[Vector[InvestorDetailsModel]])(f: Vector[InvestorDetailsModel] => Result): Result = {
     vector match {
@@ -139,6 +136,10 @@ trait ControllerHelpers {
   }
 
   def getInvestorIndex(targetIndex: Int, data: Vector[InvestorDetailsModel]): Int = {
+    data.indexWhere(_.processingId.getOrElse(0) == targetIndex)
+  }
+
+  def getShareIndex(targetIndex: Int, data: Vector[PreviousShareHoldingModel]): Int = {
     data.indexWhere(_.processingId.getOrElse(0) == targetIndex)
   }
 
@@ -154,10 +155,25 @@ trait ControllerHelpers {
     investors.lift(index).flatMap(f)
   }
 
+  def retrieveShareData[T](index: Int, shares: Option[Vector[PreviousShareHoldingModel]])(f: PreviousShareHoldingModel => Option[T]): Option[T] = {
+    shares.flatMap(_.lift(index).flatMap(f))
+  }
+
   def fillForm[T](form: Form[T], data: Option[T]): Form[T] = {
     data match {
       case Some(model) => form.fill(model)
       case _ => form
+    }
+  }
+
+  def redirectInvalidPreviousShareHolding(index: Int, shares: Option[Vector[PreviousShareHoldingModel]])(f: Int => Result): Result = {
+    shares.map { data =>
+      if (data.nonEmpty)
+        f(index)
+      else None
+    } match {
+      case Some(result: Result) => result
+      case _ => Redirect(routes.AddInvestorOrNomineeController.show(Some(index)))
     }
   }
 }
