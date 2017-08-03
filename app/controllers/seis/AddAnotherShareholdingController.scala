861c6375a5219d2976465c546dfd6b30297e2876
James Forster <james.forster@digital.hmrc.gov.uk>
2017-08-03 10:22:44 +0100

    Created stubbed routes.

---
+++ app/controllers/seis/AddAnotherShareholdingController.scala
@@ -0,0 +1,40 @@
+/*
+ * Copyright 2017 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package controllers.seis
+
+import auth.{AuthorisedAndEnrolledForTAVC, Flow, SEIS}
+import config.{AppConfig, FrontendAppConfig, FrontendAuthConnector}
+import connectors.{EnrolmentConnector, S4LConnector}
+import controllers.predicates.FeatureSwitch
+import uk.gov.hmrc.play.frontend.auth.connectors.AuthConnector
+import uk.gov.hmrc.play.frontend.controller.FrontendController
+
+object AddAnotherShareholdingController extends AddAnotherShareholdingController {
+  override lazy val enrolmentConnector: EnrolmentConnector = EnrolmentConnector
+  override lazy val applicationConfig: AppConfig = FrontendAppConfig
+  override lazy val s4lConnector: S4LConnector = S4LConnector
+  override lazy val authConnector: AuthConnector = FrontendAuthConnector
+}
+
+trait AddAnotherShareholdingController extends FrontendController with AuthorisedAndEnrolledForTAVC with FeatureSwitch {
+  override lazy val acceptedFlows: Seq[Seq[Flow]] = Seq(Seq(SEIS))
+
+  val show = TODO
+
+  val submit = TODO
+
+}
