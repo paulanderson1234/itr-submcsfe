eaa9e40f482d9d69f918a8940e2b52c0ff54bbbb
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2017-08-04 09:10:33 +0100

    TAVC-TestControllerRouting update route location to meet with expected Acceptance feature tests

--- app/testOnly/controllers/seis/TestEndpointSEISController.scala
+++ app/testOnly/controllers/seis/TestEndpointSEISController.scala
@@ -214,10 +214,10 @@ trait TestEndpointSEISController extends FrontendController with AuthorisedAndEn
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkSeventyPercentSpent, routes.TestEndpointSEISController.showPageOne(None).url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkShareDescription, routes.TestEndpointSEISController.showPageOne(None).url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkAddInvestorOrNominee, routes.TestEndpointSEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkIsExistingShareHolder, routes.TestEndpointSEISController.showPageOne(None).url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkIsExistingShareHolder, routes.TestEndpointSEISController.showPageTwo().url)
 
     //Investor Details
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkAddInvestorOrNominee, routes.TestEndpointSEISController.showPageTwo().url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkAddInvestorOrNominee, routes.TestEndpointSEISController.showPageOne(None).url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkCompanyOrIndividual, routes.TestEndpointSEISController.showPageTwo().url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkCompanyAndIndividualBoth, routes.TestEndpointSEISController.showPageTwo().url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkNumberOfSharesPurchased, routes.TestEndpointSEISController.showPageTwo().url)
@@ -225,7 +225,7 @@ trait TestEndpointSEISController extends FrontendController with AuthorisedAndEn
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkIsExistingShareHolder, routes.TestEndpointSEISController.showPageTwo().url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkShareClassAndDescription, routes.TestEndpointSEISController.showPageTwo().url)
     s4lConnector.saveFormData[String](KeystoreKeys.backLinkNumberOfPreviouslyIssuedShares, routes.TestEndpointSEISController.showPageTwo().url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkIsPreviousShareHoldingNominalValue, routes.TestEndpointSEISController.showPageOne(None).url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkIsPreviousShareHoldingNominalValue, routes.TestEndpointSEISController.showPageTwo().url)
   }
 
   private def saveSchemeType()(implicit hc: HeaderCarrier, user: TAVCUser) = {
