6cd848cbdf269e1ba92b497edd852d5857fb68c9
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2017-08-23 11:31:42 +0100

    TEST CASES UPDATE

--- test/views/seis/CheckAnswersCompanyDetailsSpec.scala
+++ test/views/seis/CheckAnswersCompanyDetailsSpec.scala
@@ -17,7 +17,7 @@
 package views.seis
 
 import models.submission.SchemeTypesModel
-import models.{CheckAnswersModel, DateOfIncorporationModel, TradeStartDateModel}
+import models._
 import models.seis.SEISCheckAnswersModel
 import org.jsoup.Jsoup
 import play.api.i18n.Messages
@@ -27,12 +27,15 @@ import views.html.seis.checkAndSubmit.CheckAnswers
 
 class CheckAnswersCompanyDetailsSpec extends CheckAnswersSpec {
 
+  val grossAssets = Some(GrossAssetsModel(12345))
+  val fullTimeEmployees = Some(FullTimeEmployeeCountModel(22))
   "The Check Answers page" should {
 
     "Verify that the Check Answers page contains the correct elements for Section 1: Company details" +
       " when it is fully populated with company detail models and had trade start date is true" in {
       val model = SEISCheckAnswersModel(Some(registeredAddressModel), Some(dateOfIncorporationModel), Some(tradeStartDateModelYes),
-        Some(natureOfBusinessModel),Vector(), None, None, None, None, None, None, None, None, None, None, None, None, None,
+        Some(natureOfBusinessModel),Vector(), None, None, Some(qualifyPrepareToTrade), Some(hasInvestmentTradeStartedModelYes),
+        Some(isSeventyPercentSpentModelYes), Some(shareIssuetDateModel), grossAssets, fullTimeEmployees, None, None, None, None, None,
         None, None, None, false)
       val page = CheckAnswers(model)(authorisedFakeRequest, applicationMessages)
       val document = Jsoup.parse(page.body)
@@ -74,14 +77,62 @@ class CheckAnswersCompanyDetailsSpec extends CheckAnswersSpec {
       companyDetailsTableTBody.select("tr").get(2).getElementById("tradeStart-link")
         .attr("href") shouldEqual controllers.seis.routes.TradeStartDateController.show().url
 
+      companyDetailsTableTBody.select("tr").get(3).getElementById("qualifyBusinessActivity-question").text() shouldBe
+        Messages("summaryQuestion.bussinessPurpose")
+      companyDetailsTableTBody.select("tr").get(3).getElementById("qualifyBusinessActivity-answer").text() shouldBe
+        qualifyPrepareToTrade.isQualifyBusinessActivity
+      companyDetailsTableTBody.select("tr").get(3).getElementById("qualifyBusinessActivity-link")
+        .attr("href") shouldEqual controllers.seis.routes.QualifyBusinessActivityController.show().url
+      //hasInvestmentTradeStarted
+      companyDetailsTableTBody.select("tr").get(4).getElementById("hasInvestmentTradeStarted-question").text() shouldBe
+        Messages("summaryQuestion.bussinessStatus")+" "+Messages("summaryQuestion.bussinessActivityStarted")
+      companyDetailsTableTBody.select("tr").get(4).getElementById("hasInvestmentTradeStarted-answer").text() shouldBe
+        hasInvestmentTradeStartedModelYes.hasInvestmentTradeStarted+" "+HasInvestmentTradeStartedModel.toDateString(hasInvestmentTradeStartedModelYes.hasInvestmentTradeStartedDay.get,
+          hasInvestmentTradeStartedModelYes.hasInvestmentTradeStartedMonth.get, hasInvestmentTradeStartedModelYes.hasInvestmentTradeStartedYear.get)
+      companyDetailsTableTBody.select("tr").get(4).getElementById("hasInvestmentTradeStarted-link")
+        .attr("href") shouldEqual controllers.seis.routes.HasInvestmentTradeStartedController.show().url
+
+      //SeventyPercentSpent
+      companyDetailsTableTBody.select("tr").get(5).getElementById("isSeventyPercentSpent-question").text() shouldBe
+        Messages("summaryQuestion.IsSeventyPercentSpent")
+      companyDetailsTableTBody.select("tr").get(5).getElementById("isSeventyPercentSpent-answer").text() shouldBe
+        isSeventyPercentSpentModelYes.isSeventyPercentSpent
+      companyDetailsTableTBody.select("tr").get(5).getElementById("isSeventyPercentSpent-link")
+        .attr("href") shouldEqual controllers.seis.routes.SeventyPercentSpentController.show().url
+
+      //shareIssueDate
+      companyDetailsTableTBody.select("tr").get(6).getElementById("shareIssueDate-question").text() shouldBe
+        Messages("summaryQuestion.shareIssueDate")
+      companyDetailsTableTBody.select("tr").get(6).getElementById("shareIssueDate-answer").text() shouldBe
+        ShareIssueDateModel.toDateString(shareIssuetDateModel.day.get, shareIssuetDateModel.month.get, shareIssuetDateModel.year.get)
+      companyDetailsTableTBody.select("tr").get(6).getElementById("shareIssueDate-link")
+        .attr("href") shouldEqual controllers.seis.routes.ShareIssueDateController.show().url
+
+      //Gross assets
+      companyDetailsTableTBody.select("tr").get(7).getElementById("grossAssets-question").text() shouldBe
+        Messages("summaryQuestion.businessGrossAssets")
+      companyDetailsTableTBody.select("tr").get(7).getElementById("grossAssets-answer").text() shouldBe
+        GrossAssetsModel.getAmountAsFormattedString(grossAssets.get.grossAmount)
+      companyDetailsTableTBody.select("tr").get(7).getElementById("grossAssets-link")
+        .attr("href") shouldEqual controllers.seis.routes.GrossAssetsController.show().url
+
+      //Fulltime employees
+      companyDetailsTableTBody.select("tr").get(8).getElementById("fullTimeEmployees-question").text() shouldBe
+        Messages("summaryQuestion.fullTimeEmployees")
+      companyDetailsTableTBody.select("tr").get(8).getElementById("fullTimeEmployees-answer").text() shouldBe
+        fullTimeEmployees.get.employeeCount.toString()
+      companyDetailsTableTBody.select("tr").get(8).getElementById("fullTimeEmployees-link")
+        .attr("href") shouldEqual controllers.seis.routes.FullTimeEmployeeCountController.show().url
+
       document.getElementById("submit").text() shouldBe Messages("page.checkAndSubmit.checkAnswers.button.confirm")
       document.body.getElementById("back-link").attr("href") shouldEqual controllers.seis.routes.SupportingDocumentsController.show().url
     }
 
     "Verify that the Check Answers page contains the correct elements for Section 1: Company details" +
       " when it is fully populated with company detail models and had trade start date is false" in {
-      val model = SEISCheckAnswersModel(Some(registeredAddressModel), Some(dateOfIncorporationModel), Some(tradeStartDateModelNo),
-        Some(natureOfBusinessModel), Vector(), None, None, None, None, None, None, None, None, None, None, None, None, None,
+      val model = SEISCheckAnswersModel(Some(registeredAddressModel), Some(dateOfIncorporationModel), Some(tradeStartDateModelYes),
+        Some(natureOfBusinessModel),Vector(), None, None, Some(qualifyPrepareToTrade), Some(hasInvestmentTradeStartedModelYes),
+        Some(isSeventyPercentSpentModelYes), Some(shareIssuetDateModel), grossAssets, fullTimeEmployees, None, None, None, None, None,
         None, None, None, false)
       val page = CheckAnswers(model)(authorisedFakeRequest, applicationMessages)
       val document = Jsoup.parse(page.body)
@@ -113,11 +164,59 @@ class CheckAnswersCompanyDetailsSpec extends CheckAnswersSpec {
         .attr("href") shouldEqual controllers.seis.routes.DateOfIncorporationController.show().url
       //Trade start date
       companyDetailsTableTBody.select("tr").get(2).getElementById("tradeStart-question").text() shouldBe
-        Messages("summaryQuestion.hasTradeStartDate")
+        Messages("summaryQuestion.hasTradeStartDate")+" "+Messages("summaryQuestion.tradeStartDate")
       companyDetailsTableTBody.select("tr").get(2).getElementById("tradeStart-answer").text() shouldBe
-        model.tradeStartDateModel.get.hasTradeStartDate
+        model.tradeStartDateModel.get.hasTradeStartDate+" "+TradeStartDateModel.toDateString(tradeStartDateModelYes.tradeStartDay.get,
+          tradeStartDateModelYes.tradeStartMonth.get, tradeStartDateModelYes.tradeStartYear.get)
       companyDetailsTableTBody.select("tr").get(2).getElementById("tradeStart-link")
         .attr("href") shouldEqual controllers.seis.routes.TradeStartDateController.show().url
+
+      companyDetailsTableTBody.select("tr").get(3).getElementById("qualifyBusinessActivity-question").text() shouldBe
+        Messages("summaryQuestion.bussinessPurpose")
+      companyDetailsTableTBody.select("tr").get(3).getElementById("qualifyBusinessActivity-answer").text() shouldBe
+        qualifyPrepareToTrade.isQualifyBusinessActivity
+      companyDetailsTableTBody.select("tr").get(3).getElementById("qualifyBusinessActivity-link")
+        .attr("href") shouldEqual controllers.seis.routes.QualifyBusinessActivityController.show().url
+      //hasInvestmentTradeStarted
+      companyDetailsTableTBody.select("tr").get(4).getElementById("hasInvestmentTradeStarted-question").text() shouldBe
+        Messages("summaryQuestion.bussinessStatus")+" "+Messages("summaryQuestion.bussinessActivityStarted")
+      companyDetailsTableTBody.select("tr").get(4).getElementById("hasInvestmentTradeStarted-answer").text() shouldBe
+        hasInvestmentTradeStartedModelYes.hasInvestmentTradeStarted+" "+HasInvestmentTradeStartedModel.toDateString(hasInvestmentTradeStartedModelYes.hasInvestmentTradeStartedDay.get,
+          hasInvestmentTradeStartedModelYes.hasInvestmentTradeStartedMonth.get, hasInvestmentTradeStartedModelYes.hasInvestmentTradeStartedYear.get)
+      companyDetailsTableTBody.select("tr").get(4).getElementById("hasInvestmentTradeStarted-link")
+        .attr("href") shouldEqual controllers.seis.routes.HasInvestmentTradeStartedController.show().url
+
+      //SeventyPercentSpent
+      companyDetailsTableTBody.select("tr").get(5).getElementById("isSeventyPercentSpent-question").text() shouldBe
+        Messages("summaryQuestion.IsSeventyPercentSpent")
+      companyDetailsTableTBody.select("tr").get(5).getElementById("isSeventyPercentSpent-answer").text() shouldBe
+        isSeventyPercentSpentModelYes.isSeventyPercentSpent
+      companyDetailsTableTBody.select("tr").get(5).getElementById("isSeventyPercentSpent-link")
+        .attr("href") shouldEqual controllers.seis.routes.SeventyPercentSpentController.show().url
+
+      //shareIssueDate
+      companyDetailsTableTBody.select("tr").get(6).getElementById("shareIssueDate-question").text() shouldBe
+        Messages("summaryQuestion.shareIssueDate")
+      companyDetailsTableTBody.select("tr").get(6).getElementById("shareIssueDate-answer").text() shouldBe
+        ShareIssueDateModel.toDateString(shareIssuetDateModel.day.get, shareIssuetDateModel.month.get, shareIssuetDateModel.year.get)
+      companyDetailsTableTBody.select("tr").get(6).getElementById("shareIssueDate-link")
+        .attr("href") shouldEqual controllers.seis.routes.ShareIssueDateController.show().url
+
+      //Gross assets
+      companyDetailsTableTBody.select("tr").get(7).getElementById("grossAssets-question").text() shouldBe
+        Messages("summaryQuestion.businessGrossAssets")
+      companyDetailsTableTBody.select("tr").get(7).getElementById("grossAssets-answer").text() shouldBe
+        GrossAssetsModel.getAmountAsFormattedString(grossAssets.get.grossAmount)
+      companyDetailsTableTBody.select("tr").get(7).getElementById("grossAssets-link")
+        .attr("href") shouldEqual controllers.seis.routes.GrossAssetsController.show().url
+
+      //Fulltime employees
+      companyDetailsTableTBody.select("tr").get(8).getElementById("fullTimeEmployees-question").text() shouldBe
+        Messages("summaryQuestion.fullTimeEmployees")
+      companyDetailsTableTBody.select("tr").get(8).getElementById("fullTimeEmployees-answer").text() shouldBe
+        fullTimeEmployees.get.employeeCount.toString()
+      companyDetailsTableTBody.select("tr").get(8).getElementById("fullTimeEmployees-link")
+        .attr("href") shouldEqual controllers.seis.routes.FullTimeEmployeeCountController.show().url
 
       document.getElementById("submit").text() shouldBe Messages("page.checkAndSubmit.checkAnswers.button.confirm")
       document.body.getElementById("back-link").attr("href") shouldEqual controllers.seis.routes.SupportingDocumentsController.show().url
@@ -145,7 +244,6 @@ class CheckAnswersCompanyDetailsSpec extends CheckAnswersSpec {
       document.body.getElementById("back-link").attr("href") shouldEqual controllers.seis.routes.SupportingDocumentsController.show().url
     }
   }
-
 
   "The Check Answers page" should {
 
