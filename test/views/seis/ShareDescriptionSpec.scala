12e830cc1e89f9738380eb06f37c9e92b0cd7a07
Paul Anderson <paul.anderson@digital.hmrc.gov.uk>
2017-09-06 10:52:39 +0100

    TAVC-3711 Sanity testing unit tests

--- test/views/seis/ShareDescriptionSpec.scala
+++ test/views/seis/ShareDescriptionSpec.scala
@@ -16,24 +16,15 @@
 
 package views.seis
 
-import auth.{MockAuthConnector, MockConfigEISFlow, MockConfigSingleFlow}
-import common.KeystoreKeys
-import controllers.seis.ShareDescriptionController
+import common.Constants
+import controllers.helpers.MockDataGenerator
 import models.ShareDescriptionModel
 import org.jsoup.Jsoup
-import org.jsoup.nodes.Document
-import org.mockito.Matchers
-import org.mockito.Mockito._
 import play.api.i18n.Messages
-import play.api.test.Helpers._
 import views.helpers.ViewSpec
 import play.api.i18n.Messages.Implicits._
 import forms.ShareDescriptionForm._
-import views.html.seis.checkAndSubmit.CheckAnswers
 import views.html.seis.shareDetails.ShareDescription
-
-import scala.concurrent.Future
-
 
 class ShareDescriptionSpec extends ViewSpec {
 
@@ -46,34 +37,68 @@ class ShareDescriptionSpec extends ViewSpec {
 
         document.title() shouldBe Messages("page.shares.shareDescription.title")
         document.getElementById("main-heading").text() shouldBe Messages("page.shares.shareDescription.heading")
+        document.getElementById("back-link").text() shouldBe Messages("common.button.back")
+        document.getElementById("back-link").attr("href") shouldBe controllers.seis.routes.HadOtherInvestmentsController.show().url
+        document.body.getElementById("progress-section").text shouldBe Messages("common.section.progress.details.three")
         document.getElementById("description-two").text() shouldBe Messages("page.shares.shareDescription.example.text")
         document.getElementById("bullet-one").text() shouldBe Messages("page.shares.shareDescription.bullet.one")
         document.getElementById("bullet-two").text() shouldBe Messages("page.shares.shareDescription.bullet.two")
         document.getElementById("bullet-three").text() shouldBe Messages("page.shares.shareDescription.bullet.three")
+        document.getElementById("desc-one").text() shouldBe Messages("page.shares.shareDescription.question.hint")
+        document.getElementsByTag("legend").text() shouldBe Messages("page.shares.shareDescription.title")
+        document.getElementsByTag("legend").hasClass("visuallyhidden") shouldBe true
         document.getElementById("labelTextId").text() shouldBe Messages("page.shares.shareDescription.heading")
+        document.getElementById("labelTextId").hasClass("visuallyhidden") shouldBe true
+        document.body.getElementById("descriptionTextArea").attr("maxlength") shouldBe s"${Constants.shortTextLimit}"
+        document.getElementsByTag("textarea").attr("name") shouldBe "descriptionTextArea"
         document.getElementById("help").text() shouldBe Messages("common.help.whereToFind")
         document.getElementById("share-description-where-to-find").text() shouldBe Messages("page.shares.ShareDescription.location")
+        document.getElementById("next").text() shouldBe Messages("common.button.snc")
       }
 
       "there is a share description model" in {
-        val shareDescriptionModel = ShareDescriptionModel("")
+        lazy val maxLengthText: String = MockDataGenerator.randomAlphanumericString(Constants.shortTextLimit)
+
+        val shareDescriptionModel = ShareDescriptionModel(maxLengthText)
         val page = ShareDescription(shareDescriptionForm.fill(shareDescriptionModel),
           controllers.seis.routes.HadOtherInvestmentsController.show().toString)(authorisedFakeRequest, applicationMessages)
         val document = Jsoup.parse(page.body)
 
+
         document.title() shouldBe Messages("page.shares.shareDescription.title")
         document.getElementById("main-heading").text() shouldBe Messages("page.shares.shareDescription.heading")
+        document.getElementById("back-link").text() shouldBe Messages("common.button.back")
+        document.getElementById("back-link").attr("href") shouldBe controllers.seis.routes.HadOtherInvestmentsController.show().url
+        document.body.getElementById("progress-section").text shouldBe Messages("common.section.progress.details.three")
         document.getElementById("description-two").text() shouldBe Messages("page.shares.shareDescription.example.text")
         document.getElementById("bullet-one").text() shouldBe Messages("page.shares.shareDescription.bullet.one")
         document.getElementById("bullet-two").text() shouldBe Messages("page.shares.shareDescription.bullet.two")
         document.getElementById("bullet-three").text() shouldBe Messages("page.shares.shareDescription.bullet.three")
+        document.getElementById("desc-one").text() shouldBe Messages("page.shares.shareDescription.question.hint")
+        document.getElementsByTag("legend").text() shouldBe Messages("page.shares.shareDescription.title")
+        document.getElementsByTag("legend").hasClass("visuallyhidden") shouldBe true
         document.getElementById("labelTextId").text() shouldBe Messages("page.shares.shareDescription.heading")
+        document.getElementById("labelTextId").hasClass("visuallyhidden") shouldBe true
+        document.body.getElementById("descriptionTextArea").attr("maxlength") shouldBe s"${Constants.shortTextLimit}"
+        document.body.getElementById("descriptionTextArea").text() shouldBe maxLengthText
+        document.getElementsByTag("textarea").attr("name") shouldBe "descriptionTextArea"
         document.getElementById("help").text() shouldBe Messages("common.help.whereToFind")
         document.getElementById("share-description-where-to-find").text() shouldBe Messages("page.shares.ShareDescription.location")
+        document.getElementById("next").text() shouldBe Messages("common.button.snc")
+      }
+
+      "Verify that the Share Description page contains the correct elements when an invalid ShareDescriptionModel is passed" in {
+        val page = ShareDescription(shareDescriptionForm.bind(Map("descriptionTextArea" -> "")),
+          controllers.seis.routes.ReviewPreviousSchemesController.show().toString)(authorisedFakeRequest, applicationMessages)
+        val document = Jsoup.parse(page.body)
+
+        document.title() shouldBe Messages("page.shares.shareDescription.title")
+        document.getElementById("back-link").attr("href") shouldBe controllers.seis.routes.ReviewPreviousSchemesController.show().url
+        document.getElementById("error-summary-display").hasClass("error-summary--show") shouldBe true
+        document.getElementById("error-summary-heading").text shouldBe Messages("common.error.summary.heading")
+        document.getElementById("descriptionTextArea-error-summary").text shouldBe Messages("common.error.fieldRequired")
+        document.getElementsByClass("error-notification").text shouldBe Messages("common.error.fieldRequired")
       }
     }
   }
-
-
-
 }
