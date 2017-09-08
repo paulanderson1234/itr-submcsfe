ca96bc7ff07b65916f6a549a26101d93c39f3029
david.gordon <david.gordon@digital.hmrc.gov.uk>
2017-09-08 10:50:33 +0100

    TAVC-3739 changed error page layout

--- test/views/eis/TradingForTooLongSpec.scala
+++ test/views/eis/TradingForTooLongSpec.scala
@@ -16,44 +16,85 @@
 
 package views.eis
 
-import auth.{MockConfigEISFlow, MockAuthConnector}
-import config.FrontendAppConfig
-import controllers.eis.TradingForTooLongController
-import controllers.seis.routes
+import controllers.helpers.FakeRequestHelper
+import controllers.eis.routes
 import org.jsoup.Jsoup
 import org.jsoup.nodes.Document
 import play.api.i18n.Messages
 import play.api.i18n.Messages.Implicits._
-import play.api.test.Helpers._
 import views.helpers.ViewSpec
+import views.html.eis.investment._
 
-class TradingForTooLongSpec extends ViewSpec {
+class TradingForTooLongSpec extends ViewSpec with FakeRequestHelper {
 
-  object TestController extends TradingForTooLongController {
-    override lazy val applicationConfig = MockConfigEISFlow
-    override lazy val authConnector = MockAuthConnector
-    override lazy val enrolmentConnector = mockEnrolmentConnector
-    override lazy val s4lConnector = mockS4lConnector
-  }
+  implicit val request = fakeRequest
 
-  "The Trading for too long error page" should {
-    "contain the correct elements" in new Setup {
-      val document: Document = {
-        val result = TestController.show.apply(authorisedFakeRequest)
-        Jsoup.parse(contentAsString(result))
+  "The Trading For Too Long error page" should {
+    lazy val view = TradingForTooLong()
+    lazy val document: Document = Jsoup.parse(view.body)
+
+    "contain the correct title" in {
+      document.title shouldBe Messages("page.investment.TradingForTooLong.title")
+    }
+
+    "contain a back link" which {
+      lazy val backLink = document.select("article > a")
+
+      "has the correct text" in {
+        backLink.text() shouldBe Messages("common.button.back")
       }
-      document.title shouldEqual Messages("page.investment.TradingForTooLong.title")
-      document.body.getElementById("main-heading").text() shouldEqual Messages("page.investment.TradingForTooLong.heading")
-      document.body.getElementById("trading-too-long-reason").text() shouldEqual Messages("page.investment.TradingForTooLong.reason")
-      document.body.getElementById("trading-too-long").text() shouldEqual Messages("page.investment.TradingForTooLong.bullet.one")
-      document.body.getElementById("not-new-business").text() shouldEqual Messages("page.investment.TradingForTooLong.bullet.two")
-      document.body.getElementById("link-text-one").attr("href") shouldEqual "https://www.gov.uk/hmrc-internal-manuals/venture-capital-schemes-manual/8154"
-      document.body.getElementById("back-link").attr("href") shouldEqual controllers.eis.routes.NewProductController.show().url
-      document.select("article div p").get(2).text() shouldBe Messages("common.changeAnswers.text") + " " + Messages("common.changeAnswers.link") + "."
-      document.select("article div p").get(2).select("a").text() shouldBe Messages("common.changeAnswers.link")
-      document.select("article div p").get(2).select("a").attr("href") shouldBe controllers.eis.routes.NewProductController.show().url
-      document.getElementById("submit").text() shouldBe Messages("common.button.continue")
-      document.select("form").attr("action") shouldBe controllers.eis.routes.TradingForTooLongController.submit().url
+
+      "has the correct link" in {
+        backLink.attr("href") shouldBe routes.NewProductController.show().url
+      }
+    }
+
+    "contain the correct heading" in {
+      document.select("h1").text() shouldBe Messages("page.investment.TradingForTooLong.heading")
+    }
+
+    "contain a description of the error" in {
+      document.select("article div p").first().text() shouldBe Messages("page.investment.TradingForTooLong.reason")
+      document.getElementById("trading-too-long").text() shouldBe Messages("page.investment.TradingForTooLong.bullet.one")
+      document.getElementById("not-new-business").text() shouldBe Messages("page.investment.TradingForTooLong.bullet.two")
+    }
+
+    "contain a change link" which {
+      lazy val changeLink = document.select("article div p").get(1)
+
+      "has the correct sentence" in {
+        changeLink.text() shouldBe Messages("common.changeAnswers.text") +
+          " " + Messages("common.changeAnswers.link") + "."
+      }
+
+      "contains the correct link text" in {
+        changeLink.select("a").text() shouldBe Messages("common.changeAnswers.link")
+      }
+
+      "contains a link to the NewProductMarket page" in {
+        changeLink.select("a").attr("href") shouldBe routes.NewProductController.show().url
+      }
+    }
+
+    "contain the correct secondary heading" in {
+      document.select("h2").text().trim shouldBe Messages("common.error.soft.secondaryHeading")
+    }
+
+    "contains the correct what next information" in {
+      document.select("article div p").get(2).text() shouldBe Messages("common.error.soft.whatNext")
+    }
+
+
+    "contain a continue button" which {
+      lazy val button = document.getElementById("next")
+
+      "contains the correct message" in {
+        button.text() shouldBe Messages("common.button.continue")
+      }
+
+      "have a form posting to the correct route" in {
+        document.select("form").attr("action") shouldBe routes.TradingForTooLongController.submit().url
+      }
     }
   }
 }
