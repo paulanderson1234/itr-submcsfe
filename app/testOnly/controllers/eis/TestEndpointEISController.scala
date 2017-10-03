97f977b5127a710b630db777d3ee0b9c1fad49e1
Ladon Jackson <ladon.jackson@digital.hmrc.gov.uk>
2017-10-03 16:16:10 +0100

    updating testEndPointEIS

--- app/testOnly/controllers/eis/TestEndpointEISController.scala
+++ app/testOnly/controllers/eis/TestEndpointEISController.scala
@@ -42,43 +42,67 @@ trait TestEndpointEISController extends FrontendController with AuthorisedAndEnr
 
   val s4lConnector: S4LConnector
   val defaultPreviousSchemesSize = 2
+  val keyStoreKeyInvestorModelOptions = "testonly:keyStoreKeyInvestorModelOptions"
 
   val kiProcessingModelYes = KiProcessingModel(Some(true), Some(true), Some(true), Some(true), Some(true), Some(true))
   val kiProcessingModelNo = KiProcessingModel(Some(false), Some(false), Some(false), Some(false), Some(false), Some(false))
 
-  def showPageOne(schemes: Option[Int]): Action[AnyContent] = AuthorisedAndEnrolled.async {
+  def showPageOne: Action[AnyContent] = AuthorisedAndEnrolled.async {
     implicit user => implicit request =>
       for {
         natureOfBusinessForm <- fillForm[NatureOfBusinessModel](KeystoreKeys.natureOfBusiness, NatureOfBusinessForm.natureOfBusinessForm)
         dateOfIncorporationForm <- fillForm[DateOfIncorporationModel](KeystoreKeys.dateOfIncorporation, DateOfIncorporationForm.dateOfIncorporationForm)
+        qualifyingBusinessForm <- fillForm[QualifyBusinessActivityModel](KeystoreKeys.isQualifyBusinessActivity,
+          QualifyBusinessActivityForm.qualifyBusinessActivityForm)
+        tradeStartDateForm <- fillForm[HasInvestmentTradeStartedModel](KeystoreKeys.tradeStartDate, HasInvestmentTradeStartedForm.hasInvestmentTradeStartedForm)
+        researchStartDateForm <- fillForm[ResearchStartDateModel](KeystoreKeys.researchStartDate, ResearchStartDateForm.researchStartDateForm)
         commercialSaleForm <- fillForm[CommercialSaleModel](KeystoreKeys.commercialSale, CommercialSaleForm.commercialSaleForm)
+        shareIssueDateForm <- fillForm[ShareIssueDateModel](KeystoreKeys.shareIssueDate, ShareIssueDateForm.shareIssueDateForm)
+        grossAssetsBeforeForm <- fillForm[GrossAssetsModel](KeystoreKeys.grossAssets, GrossAssetsForm.grossAssetsForm)
+        grossAssetsAfterForm <- fillForm[GrossAssetsAfterIssueModel](KeystoreKeys.grossAssetsAfterIssue, GrossAssetsAfterIssueForm.grossAssetsAfterIssueForm)
+        isCompanyKnowledgeIntensiveForm <- fillForm[IsCompanyKnowledgeIntensiveModel](KeystoreKeys.isCompanyKnowledgeIntensive, IsCompanyKnowledgeIntensiveForm.isCompanyKnowledgeIntensiveForm)
         isKnowledgeIntensiveForm <- fillForm[IsKnowledgeIntensiveModel](KeystoreKeys.isKnowledgeIntensive, IsKnowledgeIntensiveForm.isKnowledgeIntensiveForm)
         operatingCostsForm <- fillForm[OperatingCostsModel](KeystoreKeys.operatingCosts, TestOperatingCostsForm.testOperatingCostsForm)
         percentageStaffWithMastersForm <- fillForm[PercentageStaffWithMastersModel](KeystoreKeys.percentageStaffWithMasters,
           PercentageStaffWithMastersForm.percentageStaffWithMastersForm)
         tenYearPlanForm <- fillForm[TenYearPlanModel](KeystoreKeys.tenYearPlan, TenYearPlanForm.tenYearPlanForm)
-        hadPreviousRFIForm <- fillForm[HadPreviousRFIModel](KeystoreKeys.hadPreviousRFI, HadPreviousRFIForm.hadPreviousRFIForm)
-        previousSchemesForm <- fillPreviousSchemesForm
-        hadOtherInvestmentsForm <- fillForm[HadOtherInvestmentsModel](KeystoreKeys.hadOtherInvestments, HadOtherInvestmentsForm.hadOtherInvestmentsForm)
+
+//        hadPreviousRFIForm <- fillForm[HadPreviousRFIModel](KeystoreKeys.hadPreviousRFI, HadPreviousRFIForm.hadPreviousRFIForm)
+//        previousSchemesForm <- fillPreviousSchemesForm
+//        hadOtherInvestmentsForm <- fillForm[HadOtherInvestmentsModel](KeystoreKeys.hadOtherInvestments, HadOtherInvestmentsForm.hadOtherInvestmentsForm)
       } yield Ok(
         testOnly.views.html.eis.testEndpointEISPageOne(
           natureOfBusinessForm,
           dateOfIncorporationForm,
+          qualifyingBusinessForm,
+          tradeStartDateForm,
+          researchStartDateForm,
           commercialSaleForm,
+          shareIssueDateForm,
+          grossAssetsBeforeForm,
+          grossAssetsAfterForm,
+          isCompanyKnowledgeIntensiveForm,
           isKnowledgeIntensiveForm,
           operatingCostsForm,
           percentageStaffWithMastersForm,
-          tenYearPlanForm,
-          hadPreviousRFIForm,
-          previousSchemesForm,
-          schemes.getOrElse(defaultPreviousSchemesSize),
-          hadOtherInvestmentsForm
+          tenYearPlanForm
+
+//          hadPreviousRFIForm,
+//          previousSchemesForm,
+//          schemes.getOrElse(defaultPreviousSchemesSize),
+//          hadOtherInvestmentsForm
         )
       )
   }
 
-  def showPageTwo: Action[AnyContent] = AuthorisedAndEnrolled.async { implicit user => implicit request =>
+  def showPageTwo(schemes: Option[Int]): Action[AnyContent] = AuthorisedAndEnrolled.async { implicit user => implicit request =>
     for {
+      fullTimeEmployeeCountForm <- fillForm[FullTimeEmployeeCountModel](KeystoreKeys.fullTimeEmployeeCount,FullTimeEmployeeCountForm.fullTimeEmployeeCountForm)
+      hadPreviousRFIForm <- fillForm[HadPreviousRFIModel](KeystoreKeys.hadPreviousRFI, HadPreviousRFIForm.hadPreviousRFIForm)
+      previousSchemesForm <- fillPreviousSchemesForm
+      hadOtherInvestmentsForm <- fillForm[HadOtherInvestmentsModel](KeystoreKeys.hadOtherInvestments, HadOtherInvestmentsForm.hadOtherInvestmentsForm)
+      shareDescriptionForm <- fillForm[ShareDescriptionModel](KeystoreKeys.shareDescription,ShareDescriptionForm.shareDescriptionForm)
+      numberOfSharesForm <- fillForm[NumberOfSharesModel](KeystoreKeys.numberOfShares,NumberOfSharesForm.numberOfSharesForm)
       totalAmountRaisedForm <- fillForm[TotalAmountRaisedModel](KeystoreKeys.totalAmountRaised, TotalAmountRaisedForm.totalAmountRaisedForm)
       usedInvestmentReasonBeforeForm <- fillForm[UsedInvestmentReasonBeforeModel](KeystoreKeys.usedInvestmentReasonBefore,
         UsedInvestmentReasonBeforeForm.usedInvestmentReasonBeforeForm)
@@ -86,7 +110,11 @@ trait TestEndpointEISController extends FrontendController with AuthorisedAndEnr
       newGeographicalMarketForm <- fillForm[NewGeographicalMarketModel](KeystoreKeys.newGeographicalMarket, NewGeographicalMarketForm.newGeographicalMarketForm)
       newProductForm <- fillForm[NewProductModel](KeystoreKeys.newProduct, NewProductForm.newProductForm)
       turnoverCostsForm <- fillForm[AnnualTurnoverCostsModel](KeystoreKeys.turnoverCosts, TestTurnoverCostsForm.testTurnoverCostsForm)
+      thirtyDayRuleForm <- fillForm[ThirtyDayRuleModel](KeystoreKeys.thirtyDayRule, ThirtyDayRuleForm.thirtyDayRuleForm)
+      marketDescriptionForm <- fillForm[MarketDescriptionModel](KeystoreKeys.marketDescription, MarketDescriptionForm.marketDescriptionForm)
       investmentGrowForm <- fillForm[InvestmentGrowModel](KeystoreKeys.investmentGrow, InvestmentGrowForm.investmentGrowForm)
+      testInvestorModeOptionsForm <- fillForm[TestInvestorModeOptionsModel](keyStoreKeyInvestorModelOptions,
+        TestInvestorModeOptionsForm.testInvestorModeOptionsForm)
       confirmContactDetailsForm <- fillForm[ConfirmContactDetailsModel](KeystoreKeys.confirmContactDetails, ConfirmContactDetailsForm.confirmContactDetailsForm)
       contactDetailsForm <- fillForm[ContactDetailsModel](KeystoreKeys.manualContactDetails, ContactDetailsForm.contactDetailsForm)
       confirmCorrespondAddressForm <- fillForm[ConfirmCorrespondAddressModel](KeystoreKeys.confirmContactAddress,
@@ -94,17 +122,23 @@ trait TestEndpointEISController extends FrontendController with AuthorisedAndEnr
       contactAddressForm <- fillForm[AddressModel](KeystoreKeys.manualContactAddress, ContactAddressForm.contactAddressForm)
     } yield Ok(
       testOnly.views.html.eis.testEndpointEISPageTwo(
+        fullTimeEmployeeCountForm,
+        hadPreviousRFIForm,
+        previousSchemesForm,
+        schemes.getOrElse(defaultPreviousSchemesSize),
+        hadOtherInvestmentsForm,
+        shareDescriptionForm,
+        numberOfSharesForm,
         totalAmountRaisedForm,
         usedInvestmentReasonBeforeForm,
         previousBeforeDoFCSForm,
         newGeographicalMarketForm,
         newProductForm,
         turnoverCostsForm,
+        thirtyDayRuleForm,
+        marketDescriptionForm,
         investmentGrowForm,
-        confirmContactDetailsForm,
-        contactDetailsForm,
-        confirmCorrespondAddressForm,
-        contactAddressForm
+        testInvestorModeOptionsForm
       )
     )
   }
@@ -112,35 +146,60 @@ trait TestEndpointEISController extends FrontendController with AuthorisedAndEnr
   def submitPageOne: Action[AnyContent] = AuthorisedAndEnrolled.async { implicit user => implicit request =>
     val natureOfBusiness = bindForm[NatureOfBusinessModel](KeystoreKeys.natureOfBusiness, NatureOfBusinessForm.natureOfBusinessForm)
     val dateOfIncorporation = bindForm[DateOfIncorporationModel](KeystoreKeys.dateOfIncorporation, DateOfIncorporationForm.dateOfIncorporationForm)
+    val qualifyBusinessActivity = bindForm[QualifyBusinessActivityModel](KeystoreKeys.isQualifyBusinessActivity,
+      QualifyBusinessActivityForm.qualifyBusinessActivityForm)
+    val tradeStartDate = bindForm[HasInvestmentTradeStartedModel](KeystoreKeys.hasInvestmentTradeStarted,HasInvestmentTradeStartedForm.hasInvestmentTradeStartedForm)
+    val researchStartDate = bindForm[ResearchStartDateModel](KeystoreKeys.researchStartDate, ResearchStartDateForm.researchStartDateForm)
     val commercialSale = bindForm[CommercialSaleModel](KeystoreKeys.commercialSale, CommercialSaleForm.commercialSaleForm)
+    val shareIssuDate = bindForm[ShareIssueDateModel](KeystoreKeys.shareIssueDate, ShareIssueDateForm.shareIssueDateForm)
+    val grossAssetsBefore = bindForm[GrossAssetsModel](KeystoreKeys.grossAssets, GrossAssetsForm.grossAssetsForm)
+    val grossAssetsAfter = bindForm[GrossAssetsAfterIssueModel](KeystoreKeys.grossAssetsAfterIssue, GrossAssetsAfterIssueForm.grossAssetsAfterIssueForm)
+    val isCompanyKnowledgeIntensive = bindForm[IsCompanyKnowledgeIntensiveModel](KeystoreKeys.isCompanyKnowledgeIntensive,
+      IsCompanyKnowledgeIntensiveForm.isCompanyKnowledgeIntensiveForm)
     val isKnowledgeIntensive = bindKIForm()
     val testOperatingCosts = bindForm[OperatingCostsModel](KeystoreKeys.operatingCosts, TestOperatingCostsForm.testOperatingCostsForm)
     val percentageStaffWithMasters = bindForm[PercentageStaffWithMastersModel](KeystoreKeys.percentageStaffWithMasters,
       PercentageStaffWithMastersForm.percentageStaffWithMastersForm)
     val tenYearPlan = bindForm[TenYearPlanModel](KeystoreKeys.tenYearPlan, TenYearPlanForm.tenYearPlanForm)
-    val hadPreviousRFI = bindForm[HadPreviousRFIModel](KeystoreKeys.hadPreviousRFI, HadPreviousRFIForm.hadPreviousRFIForm)
-    val testPreviousSchemes = bindPreviousSchemesForm()
-    val hadOtherInvestments = bindForm[HadOtherInvestmentsModel](KeystoreKeys.hadOtherInvestments, HadOtherInvestmentsForm.hadOtherInvestmentsForm)
+
+//    val hadPreviousRFI = bindForm[HadPreviousRFIModel](KeystoreKeys.hadPreviousRFI, HadPreviousRFIForm.hadPreviousRFIForm)
+//    val testPreviousSchemes = bindPreviousSchemesForm()
+//    val hadOtherInvestments = bindForm[HadOtherInvestmentsModel](KeystoreKeys.hadOtherInvestments, HadOtherInvestmentsForm.hadOtherInvestmentsForm)
     saveBackLinks()
     saveSchemeType()
     Future.successful(Ok(
       testOnly.views.html.eis.testEndpointEISPageOne(
         natureOfBusiness,
         dateOfIncorporation,
+        qualifyBusinessActivity,
+        tradeStartDate,
+        researchStartDate,
         commercialSale,
+        shareIssuDate,
+        grossAssetsBefore,
+        grossAssetsAfter,
+        isCompanyKnowledgeIntensive,
         isKnowledgeIntensive,
         testOperatingCosts,
         percentageStaffWithMasters,
-        tenYearPlan,
-        hadPreviousRFI,
-        testPreviousSchemes,
-        defaultPreviousSchemesSize,
-        hadOtherInvestments
+        tenYearPlan
+
+//        hadPreviousRFI,
+//        testPreviousSchemes,
+//        defaultPreviousSchemesSize,
+//        hadOtherInvestments
       )
     ))
   }
 
   def submitPageTwo: Action[AnyContent] = AuthorisedAndEnrolled.async { implicit user => implicit request =>
+    val fullTimeEmployeeCount = bindForm[FullTimeEmployeeCountModel](KeystoreKeys.fullTimeEmployeeCount,FullTimeEmployeeCountForm.fullTimeEmployeeCountForm)
+    val hadPreviousRFI = bindForm[HadPreviousRFIModel](KeystoreKeys.hadPreviousRFI, HadPreviousRFIForm.hadPreviousRFIForm)
+    val testPreviousSchemes = bindPreviousSchemesForm()
+    val hadOtherInvestments = bindForm[HadOtherInvestmentsModel](KeystoreKeys.hadOtherInvestments, HadOtherInvestmentsForm.hadOtherInvestmentsForm)
+
+    val shareDescription = bindForm[ShareDescriptionModel](KeystoreKeys.shareDescription,ShareDescriptionForm.shareDescriptionForm)
+    val numberOfShares = bindForm[NumberOfSharesModel](KeystoreKeys.numberOfShares,NumberOfSharesForm.numberOfSharesForm)
     val totalAmountRaised = bindForm[TotalAmountRaisedModel](KeystoreKeys.totalAmountRaised, TotalAmountRaisedForm.totalAmountRaisedForm)
     val usedInvestmentReasonBefore = bindForm[UsedInvestmentReasonBeforeModel](KeystoreKeys.usedInvestmentReasonBefore,
       UsedInvestmentReasonBeforeForm.usedInvestmentReasonBeforeForm)
@@ -148,7 +207,10 @@ trait TestEndpointEISController extends FrontendController with AuthorisedAndEnr
     val newGeographicalMarket = bindForm[NewGeographicalMarketModel](KeystoreKeys.newGeographicalMarket, NewGeographicalMarketForm.newGeographicalMarketForm)
     val newProduct = bindForm[NewProductModel](KeystoreKeys.newProduct, NewProductForm.newProductForm)
     val testTurnoverCosts = bindForm[AnnualTurnoverCostsModel](KeystoreKeys.turnoverCosts, TestTurnoverCostsForm.testTurnoverCostsForm)
+    val thirtyDayRule = bindForm[ThirtyDayRuleModel](KeystoreKeys.thirtyDayRule, ThirtyDayRuleForm.thirtyDayRuleForm)
+    val marketDescription = bindForm[MarketDescriptionModel](KeystoreKeys.marketDescription, MarketDescriptionForm.marketDescriptionForm)
     val investmentGrow = bindForm[InvestmentGrowModel](KeystoreKeys.investmentGrow, InvestmentGrowForm.investmentGrowForm)
+    val investorModelOptions = bindForm[TestInvestorModeOptionsModel](keyStoreKeyInvestorModelOptions, TestInvestorModeOptionsForm.testInvestorModeOptionsForm)
     val confirmContactDetails = bindConfirmContactDetails()
     val contactDetails = bindForm[ContactDetailsModel](KeystoreKeys.manualContactDetails, ContactDetailsForm.contactDetailsForm)
     val confirmCorrespondAddress = bindConfirmContactAddress()
@@ -157,33 +219,42 @@ trait TestEndpointEISController extends FrontendController with AuthorisedAndEnr
     saveSchemeType()
     Future.successful(Ok(
       testOnly.views.html.eis.testEndpointEISPageTwo(
+        fullTimeEmployeeCount,
+        hadPreviousRFI,
+        testPreviousSchemes,
+        defaultPreviousSchemesSize,
+        hadOtherInvestments,
+        shareDescription,
+        numberOfShares,
         totalAmountRaised,
         usedInvestmentReasonBefore,
         previousBeforeDoFCS,
         newGeographicalMarket,
         newProduct,
         testTurnoverCosts,
+        thirtyDayRule,
+        marketDescription,
         investmentGrow,
-        confirmContactDetails,
-        contactDetails,
-        confirmCorrespondAddress,
-        contactAddress
+        investorModelOptions
       )
     ))
   }
 
   private def saveBackLinks()(implicit hc: HeaderCarrier, user: TAVCUser) = {
     s4lConnector.saveFormData[Boolean](KeystoreKeys.applicationInProgress, true)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkConfirmCorrespondence, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkIneligibleForKI, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkInvestmentGrow, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkNewGeoMarket, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkPreviousScheme, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkProposedInvestment, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkReviewPreviousSchemes, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkSubsidiaries, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkSubSpendingInvestment, routes.TestEndpointEISController.showPageOne(None).url)
-    s4lConnector.saveFormData[String](KeystoreKeys.backLinkSupportingDocs, routes.TestEndpointEISController.showPageOne(None).url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkFullTimeEmployeeCount, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkShareDescription, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkMarketDescription, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkConfirmCorrespondence, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkIneligibleForKI, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkInvestmentGrow, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkNewGeoMarket, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkPreviousScheme, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkProposedInvestment, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkReviewPreviousSchemes, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkSubsidiaries, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkSubSpendingInvestment, routes.TestEndpointEISController.showPageOne.url)
+    s4lConnector.saveFormData[String](KeystoreKeys.backLinkSupportingDocs, routes.TestEndpointEISController.showPageOne.url)
   }
 
   private def saveSchemeType()(implicit hc: HeaderCarrier, user: TAVCUser) = {
