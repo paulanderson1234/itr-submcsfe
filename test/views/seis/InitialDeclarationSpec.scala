926fc334eb96fd32447dd7a374f8cc174494d79f
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2017-10-04 16:20:50 +0100

    UPDATE page AND TESTS

--- test/views/seis/InitialDeclarationSpec.scala
+++ test/views/seis/InitialDeclarationSpec.scala
@@ -43,7 +43,7 @@ class InitialDeclarationSpec extends ViewSpec {
       document.getElementById("next").text() shouldBe Messages("page.initial.declaration.submit")
       document.getElementById("help-bullet-one").text() shouldBe
         Messages("page.initial.declaration.description.bullet.one") + "" +
-          " " + Messages("page.initial.declaration.description.bullet.two.seis") + " opens in a new window ."
+          " " + Messages("page.initial.declaration.description.bullet.two.seis") + " opens in a new window"
       document.select("form").attr("method") shouldBe requestType
       document.select("form").attr("action") shouldBe routes.NatureOfBusinessController.show().url
       document.getElementById("do-not-agree").text() shouldBe Messages("page.declaration.doNotAgree")
