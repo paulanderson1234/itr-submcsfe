c9fb4d47c73255cc000dd3d793def7bfa602d04a
Narendra Vijayarao <narendra.vijayarao@digital.hmrc.gov.uk>
2017-10-04 10:59:51 +0100

    ISSUES FIXED

--- test/views/seis/AcknowledgementSpec.scala
+++ test/views/seis/AcknowledgementSpec.scala
@@ -32,18 +32,18 @@ class AcknowledgementSpec extends ViewSpec {
       lazy val document = Jsoup.parse(page.body)
 
     "have the correct title" in {
-      document.title() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.title")
+      document.title() shouldBe Messages("page.checkAndSubmit.acknowledgement.title")
     }
 
     "have a transaction banner" which {
       lazy val banner = document.select("div.transaction-banner--complete")
 
       "has the correct heading" in {
-        banner.select("h1").text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.title")
+        banner.select("h1").text() shouldBe Messages("page.checkAndSubmit.acknowledgement.title")
       }
 
       "has a paragraph regarding the reference number" in {
-        banner.select("p").text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.refNumberHeading")
+        banner.select("p").text() shouldBe Messages("page.checkAndSubmit.acknowledgement.refNumberHeading")
       }
 
       "has a span containing the reference number" in {
@@ -59,23 +59,23 @@ class AcknowledgementSpec extends ViewSpec {
       }
 
       "has a list heading" in {
-        section.select("p").get(0).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.whatNext.list.heading")
+        section.select("p").get(0).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.whatNext.list.heading")
       }
 
       "has a list" which {
         lazy val list = section.select("ul")
 
         "has the correct first element" in {
-          list.select("li").get(0).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.whatNext.list.one")
+          list.select("li").get(0).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.whatNext.list.one")
         }
 
         "has the correct second element" in {
-          list.select("li").get(1).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.whatNext.list.two")
+          list.select("li").get(1).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.whatNext.list.two")
         }
       }
 
       "has a description of the outcome" in {
-        section.select("p").get(1).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.whatNext.outcome")
+        section.select("p").get(1).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.whatNext.outcome")
       }
 
       "has a description of the certificate issue process" in {
@@ -83,11 +83,11 @@ class AcknowledgementSpec extends ViewSpec {
       }
 
       "has a warning about claiming relief" in {
-        section.select("p.important-notice").text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.whatNext.certificate.warning")
+        section.select("p.important-notice").text() shouldBe Messages("page.checkAndSubmit.acknowledgement.whatNext.certificate.warning")
       }
 
       "has a description of the failed condition process" in {
-        section.select("p").get(4).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.whatNext.conditions")
+        section.select("p").get(4).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.whatNext.conditions")
       }
     }
 
@@ -95,34 +95,34 @@ class AcknowledgementSpec extends ViewSpec {
       lazy val section = document.select("div.column-two-thirds > div.form-group").get(2)
 
       "has the correct subheading" in {
-        section.select("h2").text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.supporting.docs.heading")
+        section.select("h2").text() shouldBe Messages("page.checkAndSubmit.acknowledgement.supporting.docs.heading")
       }
 
       "has the correct list heading" in {
-        section.select("p").get(0).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.review")
+        section.select("p").get(0).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.review")
       }
 
       "has a list" which {
         lazy val list = section.select("ul")
 
         "has the correct first element" in {
-          list.select("li").get(0).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.doc.one")
+          list.select("li").get(0).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.doc.one")
         }
 
         "has the correct second element" in {
-          list.select("li").get(1).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.doc.two")
+          list.select("li").get(1).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.doc.two")
         }
 
         "has the correct third element" in {
-          list.select("li").get(2).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.doc.three")
+          list.select("li").get(2).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.doc.three")
         }
 
         "has the correct fourth element" in {
-          list.select("li").get(3).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.doc.four")
+          list.select("li").get(3).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.doc.four")
         }
 
         "has the correct fifth element" in {
-          list.select("li").get(4).text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.doc.five")
+          list.select("li").get(4).text() shouldBe Messages("page.checkAndSubmit.acknowledgement.doc.five")
         }
       }
     }
@@ -131,7 +131,7 @@ class AcknowledgementSpec extends ViewSpec {
       lazy val paragraph = document.select("div.column-two-thirds > div.form-group").get(3).select("p")
 
       "has the first part of the message" in {
-        paragraph.select("span").first().text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.upload.one")
+        paragraph.select("span").first().text() shouldBe Messages("page.checkAndSubmit.acknowledgement.upload.one")
       }
 
       "has the second part of the message as a link" which {
@@ -142,12 +142,12 @@ class AcknowledgementSpec extends ViewSpec {
         }
 
         "has the correct part of the message" in {
-          link.text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.upload.link")
+          link.text() shouldBe Messages("page.checkAndSubmit.acknowledgement.upload.link")
         }
       }
 
       "has the third part of the message" in {
-        paragraph.select("span").last().text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.upload.two")
+        paragraph.select("span").last().text() shouldBe Messages("page.checkAndSubmit.acknowledgement.upload.two")
       }
 
       "has the fourth part of the message as a link" which {
@@ -158,7 +158,7 @@ class AcknowledgementSpec extends ViewSpec {
         }
 
         "has the correct part of the message" in {
-          link.text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.upload.dashboard")
+          link.text() shouldBe Messages("page.checkAndSubmit.acknowledgement.upload.dashboard")
         }
       }
     }
@@ -171,7 +171,7 @@ class AcknowledgementSpec extends ViewSpec {
       }
 
       "has the correct part of the message" in {
-        button.text() shouldBe Messages("page.seis.checkAndSubmit.acknowledgement.button.confirm")
+        button.text() shouldBe Messages("page.checkAndSubmit.acknowledgement.button.confirm")
       }
     }
   }
